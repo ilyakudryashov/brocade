23b77a2f08929e7bc1972ad3b8f026a610d136a4
ilya <ilyakydryashov@gmail.com>
2018-05-17 15:06:11 +0300

    12

--- parser1.py
+++ parser1.py
@@ -1,12 +1,17 @@
 import glob
+import os
+import csv
 
-
-in_sys_file = glob.glob('test_files/*SYS*')
+#in_sys_file = glob.glob('test_files/*SYS*')
+in_sys_file = glob.glob('*SYS*')
 print(in_sys_file[0])
+print(os.getcwd())
 
 
 def parsing(file_path, prefix):
-    """Функция создания списка из строк с префиксами передаваемых в prefix"""
+    """
+    Функция создания списка из строк с префиксами передаваемых в prefix
+    """
     result = []
     with open(file_path, "rt", encoding="utf-8") as f:  # Открываем файл
         for line in f:
@@ -25,7 +30,26 @@ def parsing(file_path, prefix):
     return result
 
 
+def csv_writer(data, file_name):
+    """
+    Запись в CSV файл
+    """
+    with open(os.getcwd()+'/'+file_name, "w", newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for line in data:
+            writer.writerow(line)
+
+
 if __name__ == '__main__':
+
+    """
     print(parsing(in_sys_file[0], "alias."))
     print(parsing(in_sys_file[0], "zone."))
+    print(csv_writer.__doc__)
+    """
+
+    alias = parsing(in_sys_file[0], "alias.")
+    zone = parsing(in_sys_file[0], "zone.")
+    csv_writer(alias, "alias.csv")
+    csv_writer(zone, "zone.csv")
 
