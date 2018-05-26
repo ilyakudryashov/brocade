dd18accdc747e8725a2adcc35be9b1f23ac130ba
ilyakudryashov <36332497+ilyakudryashov@users.noreply.github.com>
2018-05-26 17:34:42 +0300

    1

--- brocade_parser_v2.0.py
+++ brocade_parser_v2.0.py
@@ -39,73 +39,93 @@ def csv_writer(data, file_name):
             writer.writerow(line)
 
 
-def parsing_cfg_objects_and_writen_to_csv(in_file, value):
-    """
-    Парсит файл по пути переданному в file. Создает два csv файла - с алиасами или зонами.
-    :param in_file:    Необходимо передать полный путь к файлу
-    :return:        Ничего не возвращает. Создает файлы
-    """
-    find = value+'.'
-    parsing_result = parsing(in_file, find)
-    #zone = parsing(in_file, "zone.")
-    cfg = parsing(in_file, "cfg.", no_value=True)
-    csv_writer(parsing_result, cfg + "_" + value + ".csv")
-    #sv_writer(zone, cfg + "_zone.csv")
-
-
-
 def find_all_files_by_template_in_subdirs(pattern, folder=os.getcwd()):
     """
     Ищет все файлы по шаблону (pattern) в указанной папке (folder) и во всех вложенных.
     Если folder не указн то ищется в текущем каталоге
     """
     result = []
-    for root, dirs, files in os.walk(folder):
+    for dirs, subdirs, files in os.walk(folder):
         for filename in fnmatch.filter(files, pattern):
-            fullname = os.path.join(root, filename)
+            fullname = os.path.join(dirs, filename)
             result.append(fullname)
     return result
 
 
-def parsing_sshow_sys(value):
+def csv_writer(data, file_name):
     """
-    Парсим все найденные по маске *SSHOW_SYS.txt файлы (поиск alias и zone) и складываем в csv
-    :return:    создает csv файлы
+    Запись в CSV файл
     """
+    with open(os.getcwd()+'/'+file_name, "w", newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for line in data:
+            writer.writerow(line)
+
+
+def parsing_sshow_sys(value, in_csv=False):
+    """
+    Парсим все найденные по маске *SSHOW_SYS.txt файлы (поиск alias и zone) и складываем в csv, если in_csv=True
+    Если in_csv=False возращаем результат поискаю
+    :return:    возращает найденное
+    """
+    result = []
+    find = value+'.'
     for file in find_all_files_by_template_in_subdirs('*SSHOW_SYS.txt'):
-        parsing_cfg_objects_and_writen_to_csv(file, value)
+        parsing_result = parsing(file, find)
+        cfg = parsing(file, "cfg.", no_value=True)
+        if in_csv:
+            csv_writer(parsing_result, cfg + "_" + value + ".csv")
+            result = None
+        else:
+            result.append(parsing_result)
+    return result
 
 
 def load_sshow_sys(value):
     text_result.delete('1.0', 'end')
-    text_result.insert('1.0', fn)
+    text_result.insert('1.0', parsing_sshow_sys(value))
 
 
 if __name__ == '__main__':
-    #for file in find_all_files_by_template_in_subdirs('*SSHOW_SYS.txt'):
-    #    parsing_cfg_objects_and_writen_to_csv(file)
     root = tkinter.Tk()
-    frame_button = tkinter.Frame(root, bg='gray', border=5)
-    frame_text = tkinter.Frame(root, bg='gray', border=5)
+    root.title('Brocade Parser v2.0')
+    root.minsize(800, 450)
 
-    frame_button.pack(side='top', fill='x')
-    frame_text.pack(side='bottom')
+    frame_button = tkinter.Frame(root, bg='gray15', border=1, relief='raise')
+    frame_text = tkinter.LabelFrame(root, bg='gray15', border=1, relief='flat', text='Результат:', fg='sienna1')
 
-    button_find_alias = tkinter.Button(frame_button, text="Найти все alias", bg='black', fg='yellow')
-    #button_load_alias_in_text = tkinter.Button(frame_button, text="Загрузить все alias", bg='black', fg='yellow')
+    frame_button.pack(side='top', fill='both')
+    frame_text.pack(side='top', fill='both')
 
-    text_result = tkinter.Text(frame_text, font='Arial 8', wrap='word', bg='black', fg='yellow')
-    scrollbar_text_result = tkinter.Scrollbar(frame_text)
+    # ---frame_button---
+    button_alias_to_csv = tkinter.Button(frame_button, text="alias->CSV", bg='gray20', fg='sienna1', font='Arial 8',
+                                         relief='raise', overrelief='sunken', activebackground='sienna1')
+    button_zone_to_csv = tkinter.Button(frame_button, text="zone->CSV", bg='gray20', fg='sienna1', font='Arial 8',
+                                        relief='raise', overrelief='sunken', activebackground='sienna1')
+    button_load_alias_in_text = tkinter.Button(frame_button, text="показать все alias", bg='gray20', fg='sienna1',
+                                               font='Arial 8', relief='raise', overrelief='sunken',
+                                               activebackground='sienna1')
+
+    button_alias_to_csv.pack(side='left')
+    button_zone_to_csv.pack(side='left')
+    button_load_alias_in_text.pack(side='left')
+    # ------------------
+
+    # ---frame_text-----
+    text_result = tkinter.Text(frame_text, font='Arial 7', bg='gray20', fg='sienna1')
+    scrollbar_text_result = tkinter.Scrollbar(frame_text, bg='gray20', activebackground='gray20')
     scrollbar_text_result['command'] = text_result.yview
     text_result['yscrollcommand'] = scrollbar_text_result.set
 
-    button_find_alias.pack(side='left')
-    #button_load_alias_in_text.pack(side='right')
-    text_result.pack(side='left')
-    scrollbar_text_result.pack(side='right')
+    text_result.pack(side='left', fill='both')
+    scrollbar_text_result.pack(side='right', fill='y')
+    # ------------------
 
-    button_find_alias.bind("<Button-1>", lambda event: parsing_sshow_sys('alias'))
-    #button_load_alias_in_text("<Button-1>",load_sshow_sys('alias')
+    button_alias_to_csv.bind("<Button-1>", lambda event: parsing_sshow_sys('alias', in_csv=True))
+    button_zone_to_csv.bind("<Button-1>", lambda event: parsing_sshow_sys('zone', in_csv=True))
+    #button_load_alias_in_text("<Button-1>", load)
+    load_sshow_sys('alias')
 
     root.mainloop()
+
 
